12097ba9770bb190383b2ce103d401924327467d
Michal Kurzal <michalkurzal200@gmail.com>
2020-08-29 06:23:15 +0200

    .

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, NgModule } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 interface Book {
   titel: string;
@@ -18,7 +19,11 @@ interface Book {
 export class BookListComponent implements OnInit {
   title = 'books-frontend';
   books;
-  constructor(private http: HttpClient, public router: Router) {}
+  constructor(
+    private http: HttpClient,
+    public router: Router,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {
     this.http
@@ -27,6 +32,12 @@ export class BookListComponent implements OnInit {
       .then((res) => {
         this.books = res;
         console.log(this.books);
+      })
+      .catch((err) => {
+        console.log(err);
+        this._snackBar.open('No connection to API', 'Ok', {
+          duration: 20000,
+        });
       });
   }
   goforward(book) {
--- src/app/book-list/book-list.module.ts
+++ src/app/book-list/book-list.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BookListComponent } from './book-list.component';
 
 @NgModule({
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatSnackBarModule],
   declarations: [BookListComponent],
 })
 export class BookListModule {}
